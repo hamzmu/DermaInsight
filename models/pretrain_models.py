385c6941b6d6e1499f9d2af65d30b8fdc5773c29
Hamza <muhhamza24@gmail.com>
2025-03-22 00:57:24 -0400

    updates

--- models/pretrain_models.py
+++ models/pretrain_models.py
@@ -4,31 +4,30 @@ from io import BytesIO
 from IPython.display import Image as IPImage, display
 from huggingface_hub import from_pretrained_keras
 import tensorflow as tf
+import requests
 
-# Download sample image
+
 
 
 def google_derm_cnn(path: str) -> dict:
     """
+    Runs inference on the given dermatology image and returns the embedding vector.
     Parameters:
-
-
-
-
+        path (str): Path to the image.
+    Returns:
+        dict: Embedding vector.
     """
-
-
     # Load the image
-    img = Image.open("3445096909671059178.png")
+    img = Image.open(path)
     buf = BytesIO()
     img.convert('RGB').save(buf, 'PNG')
     image_bytes = buf.getvalue()
 
     # Format input
-    input_tensor= tf.train.Example(features=tf.train.Features(
-            feature={'image/encoded': tf.train.Feature(
-                bytes_list=tf.train.BytesList(value=[image_bytes]))
-            })).SerializeToString()
+    input_tensor = tf.train.Example(features=tf.train.Features(
+        feature={'image/encoded': tf.train.Feature(
+            bytes_list=tf.train.BytesList(value=[image_bytes]))
+        })).SerializeToString()
 
     # Load the model directly from Hugging Face Hub
     loaded_model = from_pretrained_keras("google/derm-foundation")
@@ -40,8 +39,13 @@ def google_derm_cnn(path: str) -> dict:
     # Extract the embedding vector
     embedding_vector = output['embedding'].numpy().flatten()
 
+    return {"embedding_vector": embedding_vector}
 
-import requests
+
+
+
+
+
 
 url = "https://storage.googleapis.com/dx-scin-public-data/dataset/images/3445096909671059178.png"
 response = requests.get(url)
